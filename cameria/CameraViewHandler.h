ed4c34097d58c718ec08f6af50f212b0eae4ba98
joamag <joamag@gmail.com>
2012-11-13 12:03:33 +0000

    new camera view handler protocol added

---
+++ cameria/CameraViewHandler.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewHandler.h
+//  cameria
+//
+//  Created by Administrator on 11/13/12.
+//  Copyright (c) 2012 ThinkFlood Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CameraViewHandler <NSObject>
+
+@end
