a54340297000ff3b6bc9035efb6a7c7a56e59aab
joamag <joamag@gmail.com>
2012-11-14 17:59:47 +0000

    initial version of filter cell

---
+++ cameria/classes/HMFilterCell.h
@@ -0,0 +1,13 @@
+//
+//  HMFilterCell.h
+//  cameria
+//
+//  Created by Administrator on 11/14/12.
+//  Copyright (c) 2012 Hive Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMFilterCell : UITableViewCell
+
+@end
